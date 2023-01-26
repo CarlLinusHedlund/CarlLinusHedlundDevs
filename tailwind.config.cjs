c4f443b7a88dd0a038ba84e37230e224df617ffd
Carl Linus Hedlund <89521533+CarlLinusHedlund@users.noreply.github.com>
2023-01-26 01:05:18 +0100

    adds font families, btn and inner

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,26 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        rubik: ['Rubik', 'sans-serif'],
+        roboto: ['Roboto Serif', 'serif']
+      },
+    },
+    colors: {
+      primaryDark: '#1A1A1A',
+      primaryWhite: '#F8F8F8',
+      primaryCoral: '#F17874'
+    },
+    fontSize: {
+      textXS: '12px',
+      textSM: '16px',
+      textBASE: '',
+      textLG: '',
+      textXL: '',
+      text2xl: ''
+    },
+
   },
   plugins: [],
 }
