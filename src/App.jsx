bb780eafc2846fcbb7bca330221b8f50ca90e876
Carl Linus Hedlund <89521533+CarlLinusHedlund@users.noreply.github.com>
2023-01-27 01:08:25 +0100

    Fixed error! Tailwind didnt respond because costum colors was added to extend in tailwindConfig

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-/* eslint-disable react/react-in-jsx-scope */
 import './index.css';
 
 function App() {
   return (
-    <div className="App bg-black h-screen">
-      <h1 className="text-red-500">Hello, im Carl!</h1>
+    <div className="App bg-primaryDark h-screen">
+      <h1 className="text-primaryWhite">Hello, im Carl!</h1>
     </div>
   );
 }
