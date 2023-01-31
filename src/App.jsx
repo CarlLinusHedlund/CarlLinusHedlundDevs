d752eb0892451ec2180b5ca525fffc1e58a89e0c
CarlLinusHedlund <linus.hedlund96@hotmail.com>
2023-01-31 12:39:23 +0100

    added som animation to header

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,14 @@ import './index.css';
 import Herosection from './components/main/herobanner';
 import Header from './components/header/Header';
 
-
-
 function App() {
   return (
     <div className="flex flex-col md:flex-row w-screen m-0">
-      <Header/>
-      <div id='main' className='w-full md:fixedWidth h-full flex flex-col'>
-        <Herosection/>
+      <Header />
+      <div id="main" className="w-full md:fixedWidth h-full flex flex-col">
+        <Herosection />
       </div>
-      
+
     </div>
   );
 }
--- src/components/main/herobanner.jsx
+++ src/components/main/herobanner.jsx
@@ -1,14 +1,12 @@
-import React from "react";
+import React from 'react';
 
 function Herosection() {
-    return(
-        <>
-        <section className='w-full h-screen bg-red-500'>
-        </section>
-        <section className='w-full h-screen bg-orange-500'>
-        </section>
-        </>
-    )
+  return (
+    <>
+      <section className="w-full h-screen bg-primaryDark" />
+      <section className="w-full h-screen bg-orange-500" />
+    </>
+  );
 }
 
 export default Herosection;
