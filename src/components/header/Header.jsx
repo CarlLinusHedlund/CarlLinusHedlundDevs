c7be4b35fe0f609bd1361ab74b08d6dca8892e3e
Carl Linus Hedlund <89521533+CarlLinusHedlund@users.noreply.github.com>
2023-02-01 01:53:04 +0100

    nav animation almost done

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,10 +4,8 @@ import React, { useRef, useState } from 'react';
 function Header() {
   const burgerRef = useRef();
   const navRef = useRef();
-
   const [isMenuClicked, setIsMenuClicked] = useState(false);
-//   const [burgerClass, setBurgerClass] = useState();
-//   const [menuClass, setMenuClass] = useState();
+  const [navListClass, setNavListClass] = useState('navList');
 
   const updateMenu = () => {
     function overlayStart() {
@@ -19,15 +17,16 @@ function Header() {
       navRef.current.classList.add('hidden');
     }
     if (!isMenuClicked) {
+      setNavListClass('navList navFade');
       burgerRef.current.classList.add('after:opacity-0');
       navRef.current.classList.add('flex');
       navRef.current.classList.remove('hidden');
       setTimeout(overlayStart, 300);
     } else {
-      burgerRef.current.classList.add('after:opacity-1');
+      setNavListClass('navList navFade opacity-0');
+      burgerRef.current.classList.remove('after:opacity-0');
       navRef.current.classList.add('opacity-0');
-      navRef.current.classList.add('opacity-1');
-      setTimeout(overlayRemove, 800);
+      setTimeout(overlayRemove, 500);
     }
     setIsMenuClicked(!isMenuClicked);
   };
@@ -41,22 +40,22 @@ function Header() {
         <div className="text-primaryWhite flex justify-center items-center">
           Logo
         </div>
-        <div onClick={updateMenu} className="flex justify-center items-center h-10 duration-800">
+        <button type="button" onClick={updateMenu} className="flex justify-center items-center h-10 duration-800">
           <div
             ref={burgerRef}
-            className="duration-1000 bg-primaryWhite h-0.5 w-[35px] after:duration-1000 relative rounded-lg after:w-[30px] after:h-0.5 after:absolute after:rounded-lg after:top-2.5 after:bg-white before:w-[40px] before:h-0.5 before:bg-white before:rounded-lg before:absolute before:bottom-2.5"
+            className="duration-500 bg-primaryWhite h-0.5 w-[35px] after:duration-500 relative rounded-lg after:w-[30px] after:h-0.5 after:absolute after:rounded-lg after:top-2.5 after:left-0 after:bg-white before:w-[40px] before:h-0.5 before:bg-white before:rounded-lg before:absolute before:bottom-2.5 before:left-0 before:duration-500"
           />
-        </div>
+        </button>
       </div>
       <div
         ref={navRef}
-        className="opacity-0 w-full md:fixedWidth h-full mt-[50px] md:mt-0 z-10 fixed top-0 right-0 bg-primaryDark duration-1000 text-[#E2E2E2] flex-col justify-between hidden"
+        className="opacity-0 w-full md:fixedWidth h-full mt-[50px] md:mt-0 z-10 fixed top-0 right-0 bg-primaryDark duration-700 text-[#cacaca] flex-col justify-between hidden"
       >
         <ul className="pl-[30px] md:pl-[80px] mt-[100px] flex flex-col gap-4">
-          <li className='font-rubik text-textLG xs:text-textXL font-extrabold capitalize hover:text-primaryWhite duration-1000'>Homo</li>
-          <li className='font-rubik text-textLG xs:text-textXL font-extrabold capitalize hover:text-primaryWhite duration-1000'>homo</li>
-          <li className='font-rubik text-textLG xs:text-textXL font-extrabold capitalize hover:text-primaryWhite duration-1000'>Homo</li>
-          <li className='font-rubik text-textLG xs:text-textXL font-extrabold capitalize hover:text-primaryWhite duration-1000'>homo</li>
+          <li className={navListClass} style={{ '--large-navigation-item-index': 1 }}>HOME</li>
+          <li className={navListClass} style={{ '--large-navigation-item-index': 2 }}>GET IN TOUCH</li>
+          <li className={navListClass} style={{ '--large-navigation-item-index': 3 }}>PROJECTS</li>
+          <li className={navListClass} style={{ '--large-navigation-item-index': 4 }}>ABOUT ME</li>
         </ul>
         <div className="pl-[30px] md:pl-[80px] mb-20">
           <div className="flex flex-row gap-10">
