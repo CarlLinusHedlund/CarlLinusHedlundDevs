a16d8e4008d38861091da51a66d74233769acb88
Carl Linus Hedlund <89521533+CarlLinusHedlund@users.noreply.github.com>
2023-02-08 00:57:21 +0100

    styling on banner

--- src/components/main/home/herobanner.jsx
+++ src/components/main/home/herobanner.jsx
@@ -4,18 +4,20 @@ function HeroBanner() {
 
   return (
     <div className='flex justify-center items-center sm:items-start pb-40 sm:pt-10 lg:pb-36 w-full relative inner'>
-      <div className="flex flex-col md:flex-row-reverse w-full h-full gap-5 md:items-center">
-        <div className='hidden lg:flex w-[30%]'>
-          <h3 className=' text-textLG text-primaryWhite font-rubik font-semibold '>
+      <div className="flex flex-col md:flex-row-reverse w-full h-full  md:items-center lg:justify-around ">
+        <div className='hidden lg:flex flex-col w-[40%] z-10 whitespace-nowrap text-primaryWhite '>
+          <h3 className=' text-textBASE   font-rubik font-medium '>
             Welcome to my Website
           </h3>
+          <h2 className=' text-textLG font-bold '><span className=' text-primaryCoral text-textLG '>Front End Developer</span>  from <br /> Oslo, Norway</h2>
+          <p className=' text-textSM whitespace-normal '>Here you can find everything about me, my latest works, skills and platforms i work with on a daily basis. </p>
         </div>
-        <div className=' lg:w-[30%] max-h-[450px] h-1/2 md:h-full w-full flex justify-center relative md:w-2/5 '>
+        <div className=' lg:w-[20%] max-h-[450px] md:h-full w-full flex justify-center relative md:w-2/5 '>
           {/* <img className='' src="./profile.png" alt="profile img" /> */}
-          <img className='h-auto max-w-[320px]' src="./profile-big.png" alt="profile image" />
+          <img className=' object-contain ' src="./profile-big.png" alt="profile image" />
         </div>
-        <div className="flex flex-col justify-center gap-10 h-full w-fit md:w-3/5 lg:w-2/5 xs:pl-4 md:pl-0">
-          <div className="w-full">
+        <div className="flex flex-col z-10 justify-center flex-nowrap gap-10 h-full w-fit md:w-3/5 lg:w-fit xs:pl-4 md:pl-0">
+          <div className="w-full  whitespace-nowrap">
               <h1 className='relative font-roboto text-textXL font-semibold text-primaryWhite leading-[4rem] xs:leading-[5rem]'>
                 Hi, <br /> <span className='text-text2xl xs:text-text3xl'>i'm <span className='text-primaryCoral text-text2xl xs:text-text3xl '>Carl</span></span>
               </h1>
@@ -34,7 +36,6 @@ function HeroBanner() {
             </div>
           </div>
         </div>
-        
       </div>
       <div className='w-36 h-8 border border-r-0 border-primaryWhite absolute bottom-14 -right-10 sm:right-10 rotate-90 flex items-center justify-between px-2 rounded-l-md '>
         <p className='font-rubik font-extralight text-primaryCoral text-textXS '>SCROLL DOWN</p>
