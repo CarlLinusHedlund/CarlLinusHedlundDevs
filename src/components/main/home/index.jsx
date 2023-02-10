3f620d449321ed31f078488b665dc55dc480336a
CarlLinusHedlund <linus.hedlund96@hotmail.com>
2023-02-10 14:56:42 +0100

    adds grid template

--- src/components/main/home/index.jsx
+++ src/components/main/home/index.jsx
@@ -8,7 +8,7 @@ function HomePage() {
       <section className="w-full min-h-fit md:h-screen bg-primaryDark px-5 md:px-8 lg:px-12 flex justify-center relative">
         <HeroBanner />
       </section>
-      <section className="w-full min-h-fit bg-primaryWhite px-5 md:px-8 lg:px-12 py-36 ">
+      <section className="w-full min-h-fit bg-primaryWhite ">
         <SkillSlider />
       </section>
     </>
--- src/components/main/home/skillSlider.jsx
+++ src/components/main/home/skillSlider.jsx
@@ -3,19 +3,25 @@ import React from 'react';
 function SkillSlider() {
   return (
     <>
-      <div className=''>
-        <div className=' grid grid-cols-4 '>
-          <div className=' bg-primaryDark w-20 h-20 rounded-lg flex items-center justify-center '>
-            <img src="../css.png" alt="css" />
+      <div className='px-5 md:px-8 lg:px-12 py-16 md:py-36 flex justify-center'>
+        <div className=' grid grid-cols-4 grid-row-2 justify-items-center gap-6 sm:gap-x-16 sm:gap-y-8 lg:gap-x-32 max-w-xs sm:max-w-md w-fit '>
+          <div className=' bg-primaryDark p-3 h-12 w-12 xs:h-14 xs:w-14 sm:w-20 sm:h-20 rounded-lg flex items-center justify-center '>
+            <img className='object-contain' src="../css.png" alt="css" />
           </div>
-          <div className=' bg-primaryDark w-20 h-20 rounded-lg '>
-            <img src="../css.png" alt="css" />
-          </div>
-          <div className=' bg-primaryDark w-20 h-20 rounded-lg '>
-            <img src="../css.png" alt="css" />
-          </div>
-          <div className=' bg-primaryDark w-20 h-20 rounded-lg '>
-            <img src="../css.png" alt="css" />
+          <div className=' bg-primaryDark p-3 h-12 w-12 xs:h-14 xs:w-14 sm:w-20 sm:h-20 rounded-lg flex items-center justify-center '>
+            <img className='object-contain' src="../css.png" alt="css" />
+          </div><div className=' bg-primaryDark p-3 h-12 w-12 xs:h-14 xs:w-14 sm:w-20 sm:h-20 rounded-lg flex items-center justify-center '>
+            <img className='object-contain' src="../css.png" alt="css" />
+          </div><div className=' bg-primaryDark p-3 h-12 w-12 xs:h-14 xs:w-14 sm:w-20 sm:h-20 rounded-lg flex items-center justify-center '>
+            <img className='object-contain' src="../css.png" alt="css" />
+          </div><div className=' bg-primaryDark p-3 h-12 w-12 xs:h-14 xs:w-14 sm:w-20 sm:h-20 rounded-lg flex items-center justify-center '>
+            <img className='object-contain' src="../css.png" alt="css" />
+          </div><div className=' bg-primaryDark p-3 h-12 w-12 xs:h-14 xs:w-14 sm:w-20 sm:h-20 rounded-lg flex items-center justify-center '>
+            <img className='object-contain' src="../css.png" alt="css" />
+          </div><div className=' bg-primaryDark p-3 h-12 w-12 xs:h-14 xs:w-14 sm:w-20 sm:h-20 rounded-lg flex items-center justify-center '>
+            <img className='object-contain' src="../css.png" alt="css" />
+          </div><div className=' bg-primaryDark p-3 h-12 w-12 xs:h-14 xs:w-14 sm:w-20 sm:h-20 rounded-lg flex items-center justify-center '>
+            <img className='object-contain' src="../css.png" alt="css" />
           </div>
         </div>
       </div>
