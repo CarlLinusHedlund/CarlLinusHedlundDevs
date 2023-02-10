0ebff75697c6599b496445be44914b841d1fe13f
CarlLinusHedlund <linus.hedlund96@hotmail.com>
2023-02-10 11:04:27 +0100

    initial commit

--- src/components/main/home/index.jsx
+++ src/components/main/home/index.jsx
@@ -7,7 +7,7 @@ function HomePage() {
       <section className="w-full min-h-fit md:h-screen bg-primaryDark px-5 md:px-8 lg:px-12 flex justify-center relative">
         <HeroBanner />
       </section>
-      <section className="h-screen w-full bg-primaryWhite" />
+      <section className="w-full min-h-fit bg-primaryWhite px-5 md:px-8 lg:px-12 " />
     </>
   );
 }
