cd2481ef2318c4b4b2f06be84f1fb1fe6420d733
Carl Linus Hedlund <89521533+CarlLinusHedlund@users.noreply.github.com>
2023-02-12 20:34:21 +0100

    addsa bout section

---
+++ src/components/main/home/aboutSection.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function AboutSection() {
+  return(
+    <>
+    <div className=" max-w-5xl mx-auto  py-44 ">
+      <h2 className="">About Section</h2>
+    </div>
+    </>
+  )
+}
+
+export default AboutSection;
--- src/components/main/home/home.jsx
+++ src/components/main/home/home.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import AboutSection from './aboutSection';
 import HeroBanner from './herobanner';
 import RecentProjects from './recentProjects';
 import SkillSlider from './skillSlider';
@@ -11,8 +12,14 @@ function HomePage() {
       </section>
       <section className="w-full min-h-fit bg-primaryWhite innerShadowContainer ">
         <SkillSlider />
+      </section>
+      <section className=' bg-primaryDark py-32 sm:py-44 font-rubik  '>
         <RecentProjects />
       </section>
+      <section className=' w-full min-h-fit bg-primaryWhite innerShadowContainer '>
+        <AboutSection />
+      </section>
+      
     </>
   );
 }
