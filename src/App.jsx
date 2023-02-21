c089817c829108ac5bea76dc5340a7173ecf2e88
CarlLinusHedlund <linus.hedlund96@hotmail.com>
2023-02-21 12:58:17 +0100

    router update

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './index.css';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/header/Header';
-import AboutPage from './components/main/about';
+import AboutPage from './components/main/about/about';
 import ContactPage from './components/main/contact/contact';
 import ProjectPage from './components/main/projects';
 import AdminPage from './components/main/admin/admin';
@@ -18,10 +18,10 @@ function App() {
         <div id="main" className="md:fixedWidth flex h-full w-full flex-col">
           <Routes>
             <Route path="/" element={<HomePage />} exact />
-            <Route path="/about" element={<AboutPage />} />
-            <Route path="/projects" element={<ProjectPage />} />
-            <Route path="/contact" element={<ContactPage />} />
-            <Route path="/admin" element={<AdminPage />} />
+            <Route path="/about" element={<AboutPage />} exact />
+            <Route path="/projects" element={<ProjectPage />} exact />
+            <Route path="/contact" element={<ContactPage />} exact />
+            <Route path="/admin" element={<AdminPage />} exact />
           </Routes>
         </div>
       </div>
