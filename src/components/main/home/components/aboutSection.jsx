97b03920f77c953e9ddf2d0c981dccdd229007cb
CarlLinusHedlund <linus.hedlund96@hotmail.com>
2023-02-24 13:49:32 +0100

    adds auth file

--- src/components/main/home/components/aboutSection.jsx
+++ src/components/main/home/components/aboutSection.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
 import Typed from 'typed.js';
 
+
 function AboutSection() {
   const el = useRef(null);
 
