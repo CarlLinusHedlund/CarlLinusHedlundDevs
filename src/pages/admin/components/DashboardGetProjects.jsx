ce60896c4a10ca1e42ecdb99b6738b1d55233dde
Carl Linus Hedlund <89521533+CarlLinusHedlund@users.noreply.github.com>
2023-03-06 01:57:55 +0100

    adds card component

--- src/pages/admin/components/DashboardGetProjects.jsx
+++ src/pages/admin/components/DashboardGetProjects.jsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { supabase } from '../../../supabase';
+import Cards from './ProjectsCards';
 
 function DashboardViewLists() {
-  return <div>dashboardGetProjects</div>;
+  useEffect(() => {
+    (async function getPosts() {
+      const { data, error } = await supabase.from('projects').select('*');
+      if (data) {
+        console.log(data);
+      }
+      if (error) {
+        console.log(error);
+      }
+    })();
+  }, []);
+
+  return (
+    <>
+      <div className=" w-full max-w-5xl px-10 py-20  ">
+        <Cards />
+      </div>
+    </>
+  );
 }
 
 export default DashboardViewLists;
---
+++ src/pages/admin/components/ProjectsCards.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Cards() {
+  return <div>ProjectsCards</div>;
+}
+
+export default Cards;
