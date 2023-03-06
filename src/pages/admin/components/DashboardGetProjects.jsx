a869b8518ec2bee59afe5059bd7635fdc14ed6c6
CarlLinusHedlund <linus.hedlund96@hotmail.com>
2023-03-06 16:57:35 +0100

    adds fetch and styling to view posts admin

--- src/pages/admin/components/DashboardGetProjects.jsx
+++ src/pages/admin/components/DashboardGetProjects.jsx
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { supabase } from '../../../supabase';
+import Card from './PostCard';
 
 function DashboardViewLists() {
-  return <div>dashboardGetProjects</div>;
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    (async function name() {
+      const { data, error } = await supabase.from('projects').select('*');
+      if (data) {
+        console.log(data);
+        setPosts(data);
+      }
+      if (error) {
+        console.log(error);
+      }
+    })();
+  }, []);
+
+  return (
+    <div className="flex max-w-5xl flex-col gap-10 px-10 py-20 ">
+      {posts.map((post) => (
+        <Card key={post.id} {...post} />
+      ))}
+    </div>
+  );
 }
 
 export default DashboardViewLists;
