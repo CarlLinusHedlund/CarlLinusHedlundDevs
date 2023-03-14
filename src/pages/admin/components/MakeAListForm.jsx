020117fec7d90dee871a0b2a753083ab31240983
CarlLinusHedlund <linus.hedlund96@hotmail.com>
2023-03-15 00:01:15 +0100

    some eslint fixes

--- src/pages/admin/components/MakeAListForm.jsx
+++ src/pages/admin/components/MakeAListForm.jsx
@@ -18,6 +18,7 @@ function Form() {
     console.log('VALIDATION SUCCESS');
     try {
       // Uploads img o bucket
+      // eslint-disable-next-line wrap-iife
       (async function uploadImg() {
         images.forEach(async (file) => {
           const newName = Date.now() + file.name;
@@ -28,9 +29,9 @@ function Form() {
           if (data) {
             console.log('success!!');
             console.log(data);
-            createPost();
+            createPost(values, actions);
             setImages([]);
-            actions.resetForm();
+            actions.resetForm(actions);
 
             const template = {
               url: `${
@@ -47,43 +48,10 @@ function Form() {
           }
         });
       })();
-
       // Post request to add to table
-      function createPost() {
-        supabase
-          .from('projects')
-          .insert({
-            title: values.title,
-            course: values.course,
-            description: values.description,
-            progress: values.progress,
-            tags: [values.tags],
-            images: [uploads.url],
-            active: values.active,
-          })
-          .then((response) => {
-            console.log(response);
-            if (response.data) {
-              console.log('CREATE POST SUCCEES!!');
-              actions.resetForm();
-            }
-            if (response.error) {
-              console.log('CREATE POST FAILED!!!');
-              const { data, error } = supabase.storage
-                .from('photos')
-                .remove([imageURLs]);
-              if (error) {
-                console.log(error);
-                console.log('IMAGES not removed');
-              }
-              if (data) {
-                console.log(data);
-                console.log('IMAGES removed');
-              }
-            }
-          });
-      }
-    } catch (error) {}
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   useEffect(() => {
@@ -122,6 +90,41 @@ function Form() {
       onSubmit,
     });
 
+  function createPost(actions) {
+    supabase
+      .from('projects')
+      .insert({
+        title: values.title,
+        course: values.course,
+        description: values.description,
+        progress: values.progress,
+        tags: [values.tags],
+        images: [uploads.url],
+        active: values.active,
+      })
+      .then((response) => {
+        console.log(response);
+        if (response.data) {
+          console.log('CREATE POST SUCCEES!!');
+          actions.resetForm();
+        }
+        if (response.error) {
+          console.log('CREATE POST FAILED!!!');
+          const { data, error } = supabase.storage
+            .from('photos')
+            .remove([imageURLs]);
+          if (error) {
+            console.log(error);
+            console.log('IMAGES not removed');
+          }
+          if (data) {
+            console.log(data);
+            console.log('IMAGES removed');
+          }
+        }
+      });
+  }
+
   return (
     <form
       onSubmit={handleSubmit}
@@ -133,13 +136,11 @@ function Form() {
             Information
           </h2>
           <div className="flex h-full w-full flex-col gap-5">
-            <div className=" flex flex-col ">
-              <label
-                htmlFor="title"
-                className="text-textBASE font-normal text-primaryWhite"
-              >
-                Title
-              </label>
+            <label
+              htmlFor="title"
+              className="flex flex-col text-textBASE font-normal text-primaryWhite"
+            >
+              Title
               <input
                 className={` border-b bg-transparent p-3 text-primaryWhite outline-none duration-300 focus:scale-105 focus:border-b-2 focus:outline-none lg:p-2 ${
                   errors.title && touched.title
@@ -163,14 +164,12 @@ function Form() {
               >
                 {errors.title ? `${errors.title}` : '.'}
               </label>
-            </div>
-            <div className=" flex flex-col ">
-              <label
-                htmlFor="course"
-                className="text-textBASE font-normal text-primaryWhite"
-              >
-                Course
-              </label>
+            </label>
+            <label
+              htmlFor="course"
+              className="flex flex-col text-textBASE font-normal text-primaryWhite"
+            >
+              Course
               <input
                 onChange={handleChange}
                 onBlur={handleBlur}
@@ -194,168 +193,162 @@ function Form() {
               >
                 {errors.course ? `${errors.course}` : '.'}
               </label>
-            </div>
-          </div>
-          <div className=" flex flex-col gap-2 pt-5">
-            <label
-              htmlFor="description"
-              className="text-textBASE font-normal text-primaryWhite"
-            >
-              Description
             </label>
-            <textarea
-              value={values.description}
-              onChange={handleChange}
-              onBlur={handleBlur}
-              className={`border-b bg-transparent p-3 text-primaryWhite outline-none duration-300 focus:scale-105 focus:border-b-2 focus:outline-none lg:p-2 ${
-                errors.description && touched.description
-                  ? 'border-red-400 '
-                  : 'border-primaryWhite'
-              }`}
-              name="description"
-              id="title"
-            />
-            <label
-              htmlFor="description"
-              className={` text-textXS duration-500 ${
-                errors.description && touched.description
-                  ? 'text-red-400 opacity-100'
-                  : 'text-primaryDark opacity-0 duration-75'
-              } `}
-            >
-              {errors.description ? `${errors.description}` : '.'}
-            </label>
-          </div>
-          <div className=" flex flex-col gap-2 pb-5">
-            <label
-              htmlFor="progress"
-              className="text-textBASE font-normal text-primaryWhite"
-            >
-              Progress
-            </label>
-            <textarea
-              value={values.progress}
-              onChange={handleChange}
-              onBlur={handleBlur}
-              className={`border-b bg-transparent p-2 text-primaryWhite outline-none duration-300 focus:scale-105 focus:border-b-2 focus:outline-none lg:p-2 ${
-                errors.progress && touched.progress
-                  ? 'border-red-400 '
-                  : 'border-primaryWhite'
-              }`}
-              name="progress"
-              id="progress"
-            />
-            <label
-              htmlFor="progress"
-              className={` text-textXS duration-500 ${
-                errors.progress && touched.progress
-                  ? 'text-red-400 opacity-100'
-                  : 'text-primaryDark opacity-0 duration-75'
-              } `}
-            >
-              {errors.progress ? `${errors.progress}` : '.'}
-            </label>
-          </div>
-          <div className=" flex flex-col gap-1">
-            <label
-              htmlFor="tags"
-              className="text-textBASE font-normal text-primaryWhite"
-            >
-              Add Tags
-            </label>
-            <input
-              placeholder="css, html, react"
-              type="text"
-              className={`border-b bg-transparent p-3 text-primaryWhite outline-none duration-300 focus:scale-105 focus:border-b-2 focus:outline-none lg:p-2 ${
-                errors.tags && touched.tags
-                  ? 'border-red-400 '
-                  : 'border-primaryWhite'
-              } `}
-              name="tags"
-              value={values.tags}
-              id="tags"
-              onChange={handleChange}
-              onBlur={handleBlur}
-            />
-            <label
-              htmlFor="tags"
-              className={` text-textXS duration-500 ${
-                errors.tags && touched.tags
-                  ? 'text-red-400 opacity-100'
-                  : 'text-primaryDark opacity-0 duration-75'
-              } `}
-            >
-              {errors.tags ? `${errors.tags}` : '.'}
-            </label>
-          </div>
-          <div className=" flex flex-col gap-1">
-            <label
-              htmlFor="active"
-              className="text-textBASE font-normal text-primaryWhite"
-            >
-              Active
-            </label>
-            <select
-              className=" rounded-lg px-2 py-3 font-medium duration-300 focus:scale-105 focus:outline-none"
-              name="active"
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.active}
-              id="active"
-            >
-              <option value>Active</option>
-              <option value={false}>Not Active</option>
-            </select>
           </div>
         </div>
-        <div className=" flex h-full w-full flex-col gap-10 text-textBASE font-semibold lg:w-1/2 ">
-          <h2 className=" text-textLG font-semibold capitalize text-primaryWhite ">
-            Images
+        <label
+          htmlFor="description"
+          className="flex flex-col gap-2 pt-5 text-textBASE font-normal text-primaryWhite"
+        >
+          Description
+          <textarea
+            value={values.description}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            className={`border-b bg-transparent p-3 text-primaryWhite outline-none duration-300 focus:scale-105 focus:border-b-2 focus:outline-none lg:p-2 ${
+              errors.description && touched.description
+                ? 'border-red-400 '
+                : 'border-primaryWhite'
+            }`}
+            name="description"
+            id="title"
+          />
+          <label
+            htmlFor="description"
+            className={` text-textXS duration-500 ${
+              errors.description && touched.description
+                ? 'text-red-400 opacity-100'
+                : 'text-primaryDark opacity-0 duration-75'
+            } `}
+          >
+            {errors.description ? `${errors.description}` : '.'}
+          </label>
+        </label>
+        <label
+          htmlFor="progress"
+          className="flex flex-col gap-2 pb-5 text-textBASE font-normal text-primaryWhite"
+        >
+          Progress
+          <textarea
+            value={values.progress}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            className={`border-b bg-transparent p-2 text-primaryWhite outline-none duration-300 focus:scale-105 focus:border-b-2 focus:outline-none lg:p-2 ${
+              errors.progress && touched.progress
+                ? 'border-red-400 '
+                : 'border-primaryWhite'
+            }`}
+            name="progress"
+            id="progress"
+          />
+          <label
+            htmlFor="progress"
+            className={` text-textXS duration-500 ${
+              errors.progress && touched.progress
+                ? 'text-red-400 opacity-100'
+                : 'text-primaryDark opacity-0 duration-75'
+            } `}
+          >
+            {errors.progress ? `${errors.progress}` : '.'}
+          </label>
+        </label>
+        <label
+          htmlFor="tags"
+          className="flex flex-col gap-1 text-textBASE font-normal text-primaryWhite"
+        >
+          Add Tags
+          <input
+            placeholder="css, html, react"
+            type="text"
+            className={`border-b bg-transparent p-3 text-primaryWhite outline-none duration-300 focus:scale-105 focus:border-b-2 focus:outline-none lg:p-2 ${
+              errors.tags && touched.tags
+                ? 'border-red-400 '
+                : 'border-primaryWhite'
+            } `}
+            name="tags"
+            value={values.tags}
+            id="tags"
+            onChange={handleChange}
+            onBlur={handleBlur}
+          />
+          <label
+            htmlFor="tags"
+            className={` text-textXS duration-500 ${
+              errors.tags && touched.tags
+                ? 'text-red-400 opacity-100'
+                : 'text-primaryDark opacity-0 duration-75'
+            } `}
+          >
+            {errors.tags ? `${errors.tags}` : '.'}
+          </label>
+        </label>
+        <label
+          htmlFor="active"
+          className="flex flex-col gap-1 text-textBASE font-normal text-primaryWhite"
+        >
+          Active
+          <select
+            className=" rounded-lg px-2 py-3 font-medium duration-300 focus:scale-105 focus:outline-none"
+            name="active"
+            onChange={handleChange}
+            onBlur={handleBlur}
+            value={values.active}
+            id="active"
+          >
+            <option value>Active</option>
+            <option value={false}>Not Active</option>
+          </select>
+        </label>
+      </div>
+      <div className=" flex h-full w-full flex-col gap-10 text-textBASE font-semibold lg:w-1/2 ">
+        <h2 className=" text-textLG font-semibold capitalize text-primaryWhite ">
+          Images
+        </h2>
+        <div className="flex flex-col gap-5">
+          <h2 className=" text-textBASE font-semibold capitalize text-primaryWhite ">
+            Gallery
           </h2>
-          <div className="flex flex-col gap-5">
-            <h2 className=" text-textBASE font-semibold capitalize text-primaryWhite ">
-              Gallery
-            </h2>
-            <input
-              className=" text-SM font-light text-primaryWhite "
-              multiple
-              id="image"
-              name="image"
-              type="file"
-              accept="image/png, image/jpeg"
-              onChange={onImageChange}
-            />
-            <div className="mt-4 flex flex-wrap gap-3">
-              {imageURLs.map((imageSrc) => (
-                <img
-                  key={imageSrc}
-                  className="h-16 w-auto rounded-md duration-500 hover:z-10 hover:scale-125"
-                  src={imageSrc}
-                />
-              ))}
-            </div>
+          <input
+            className=" text-SM font-light text-primaryWhite "
+            multiple
+            id="image"
+            name="image"
+            type="file"
+            accept="image/png, image/jpeg"
+            onChange={onImageChange}
+          />
+          <div className="mt-4 flex flex-wrap gap-3">
+            {imageURLs.map((imageSrc) => (
+              <img
+                key={imageSrc}
+                className="h-16 w-auto rounded-md duration-500 hover:z-10 hover:scale-125"
+                src={imageSrc}
+                alt="previewimg"
+              />
+            ))}
           </div>
-          <div className="flex flex-col gap-5">
-            <h2 className=" text-textBASE font-semibold capitalize text-primaryWhite ">
-              Main Image
-            </h2>
-            <input
-              className=" text-SM font-light text-primaryWhite "
-              id="image"
-              name="image"
-              type="file"
-              accept="image/png, image/jpeg"
-              onChange={onMainImageChange}
-            />
-            <div className="mt-4 flex flex-wrap gap-3">
-              {mainImagePreview && (
-                <img
-                  key={mainImagePreview}
-                  className="h-32 w-auto rounded-md duration-500 hover:z-10 hover:scale-125"
-                  src={mainImagePreview}
-                />
-              )}
-            </div>
+        </div>
+        <div className="flex flex-col gap-5">
+          <h2 className=" text-textBASE font-semibold capitalize text-primaryWhite ">
+            Main Image
+          </h2>
+          <input
+            className=" text-SM font-light text-primaryWhite "
+            id="image"
+            name="image"
+            type="file"
+            accept="image/png, image/jpeg"
+            onChange={onMainImageChange}
+          />
+          <div className="mt-4 flex flex-wrap gap-3">
+            {mainImagePreview && (
+              <img
+                key={mainImagePreview}
+                className="h-32 w-auto rounded-md duration-500 hover:z-10 hover:scale-125"
+                src={mainImagePreview}
+                alt="previewimg"
+              />
+            )}
           </div>
         </div>
       </div>
